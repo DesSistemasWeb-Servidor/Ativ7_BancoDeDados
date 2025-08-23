bbac9a6eeaf8213e608a2719595b2fd17673d7cf
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-08-23 14:30:53 -0300

    Conectando o bootstrap

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 # A very simple Flask Hello World app for you to get started with...
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
