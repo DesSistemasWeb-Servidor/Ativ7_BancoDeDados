f447baf5ee2da695d8c6f4c31a3f0ba17d98845f
GiovannaKarolline <mennezes.giovanna@gmail.com>
2025-09-21 20:46:37 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -70,8 +70,8 @@ def index():
         else:
             session['known'] = True
         session['name'] = form.name.data
-        usuarios = [User.query.all()]
         return redirect(url_for('index'))
+    usuarios = [User.query.all()]
     return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False), usuarios = usuarios)
 
 @app.route('/login', methods=['GET','POST'])
